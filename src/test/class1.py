2de2dc0dc905ac90b866eed1e643e41bbffbe062
cl-ire <s85609@htw-dresden.de>
2023-11-14 17:50:25 +0100

    testing out multiprocessing

---
+++ src/test/class1.py
@@ -0,0 +1,11 @@
+
+# class1.py
+
+class class2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    
+    def method1(self):
+        return self.x
---
+++ src/test/main.py
@@ -0,0 +1,7 @@
+
+from class1 import *
+
+object1 = class2("test", 1)
+
+print(object1.method1())
+
