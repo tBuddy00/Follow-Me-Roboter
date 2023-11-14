de645ef73e81633f8a7f427fc94911bb4a98de9c
cl-ire <s85609@htw-dresden.de>
2023-11-14 21:16:48 +0100

    multiprocesing test 2

--- src/test/parallel.py
+++ src/test/parallel.py
@@ -2,15 +2,13 @@ from multiprocessing import Process, Value, Array
 from time import sleep
 
 
-results_1 = []
 Storage = Value('i',0)
 
 def f(n):
     x = 0
     while True:
         x += 1
-        n.value = x
-        
+        n.value = x        
         sleep(5)
         
 
@@ -23,13 +21,10 @@ def print1(n):
 if __name__ == '__main__':
     p1 = Process(target=f, args=(Storage,))
     p2 = Process(target=print1, args=(Storage,))
-
-    
-
+        
     p1.start()
     p2.start()
-    
-    sleep(15)
 
     p1.join()
     p2.join()
+    
